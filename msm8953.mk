b66a87558d7011a502b45daf67553d172524e4b7
Isaac Chen <isaacchen@isaacchen.cn>
2019-01-19 12:34:05 +0530

    rosy: convert fingerprint wrapper into a HIDL service
    Signed-off-by: Isaac Chen <isaacchen@isaacchen.cn>

--- msm8953.mk
+++ msm8953.mk
@@ -172,6 +172,10 @@ PRODUCT_PACKAGES += \
     ethertypes \
     libebtc
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_rosy
+
 # FM
 PRODUCT_PACKAGES += \
     FMRadio \
