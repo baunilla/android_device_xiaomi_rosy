4bbaadfdfa0ab31b6a47a03ec7cb983a3135232a
Wei Wang <wvw@google.com>
2019-01-24 14:51:14 +0530

    rosy: add SysUI to PRODUCT_DEXPREOPT_SPEED_APPS
    Test: Build
    Change-Id: I088938462b389e1ebe783997f773dc5946345666

--- device.mk
+++ device.mk
@@ -132,6 +132,10 @@ PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
     android.hardware.ir@1.0-service
 
+# Dexpreopt - SystemUI
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SystemUIGoogle
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
