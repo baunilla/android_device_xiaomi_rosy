13ed9016b7d58fc0a6d2b3270cafe0f5d8a6e1a1
Han Wang <416810799@qq.com>
2019-03-24 10:07:20 +0100

    msm8953-common: Drop system property AudioFlinger client heap size
     * This is wrong. The original commit message is for a 1-GB
       RAM device. We have at least 16MB heap size by default.
    
    This reverts commit 7341640d6051dd293029c7424c55a74d64653316.
    
    Change-Id: I7eea6af39cdad7f1e20c78e0d62ce9ef8b346e78

--- prop.mk
+++ prop.mk
@@ -14,7 +14,6 @@ persist.vendor.audio.fluence.speaker=true \
 persist.vendor.audio.fluence.voicecall=true \
 persist.vendor.audio.fluence.voicerec=false \
 persist.vendor.btstack.enable.splita2dp=false \
-ro.af.client_heap_size_kbyte=7168 \
 ro.config.media_vol_steps=25 \
 ro.config.vc_call_vol_steps=15 \
 ro.vendor.audio.sdk.fluencetype=fluence \
