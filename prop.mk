b403c903fe0b0a43988dd54b3bb459960b46d908
NullDecoder <NullDecoder@users.noreply.github.com>
2019-07-05 17:56:52 +0530

    rosy: properties: update some audio values from stock

--- prop.mk
+++ prop.mk
@@ -4,15 +4,17 @@
 
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
-af.fast_track_multiplier=2 \
+af.fast_track_multiplier=1 \
 audio.deep_buffer.media=true \
 audio.offload.disable=true \
 audio.offload.min.duration.secs=30 \
 audio.offload.video=true \
+persist.vendor.audio.hw.binder.size_kbyte=1024 \
 persist.vendor.audio.fluence.speaker=true \
 persist.vendor.audio.fluence.voicecall=true \
 persist.vendor.audio.fluence.voicerec=false \
 persist.vendor.btstack.enable.splita2dp=false \
+ro.af.client_heap_size_kbyte=7168 \
 ro.config.media_vol_steps=25 \
 ro.config.vc_call_vol_steps=15 \
 ro.vendor.audio.sdk.fluencetype=fluence \
@@ -23,8 +25,8 @@ vendor.audio.offload.gapless.enabled=true \
 vendor.audio.offload.multiaac.enable=true \
 vendor.audio.offload.multiple.enabled=false \
 vendor.audio.offload.passthrough=false \
-vendor.audio.offload.track.enable=true \
-vendor.audio.parser.ip.buffer.size=262144 \
+vendor.audio.offload.track.enable=false \
+vendor.audio.parser.ip.buffer.size=0 \
 vendor.audio.playback.mch.downsample=true \
 vendor.audio.pp.asphere.enabled=false \
 vendor.audio.safx.pbe.enabled=true \
