47958c2001a9afb368f52a281fc435b26bf4448b
baunilla <baunilla@tuta.io>
2019-08-22 16:45:17 +0100

    rosy: update target kernel config

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,7 +47,7 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_SOURCE := kernel/xiaomi/rosy
-TARGET_KERNEL_CONFIG := rosy-doge_defconfig
+TARGET_KERNEL_CONFIG := rosy-baunil_defconfig
 TARGET_KERNEL_VERSION := 3.18
 
 # ANT
