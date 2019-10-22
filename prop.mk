22f2250a11e92039986a679c1a6ac48b240a5110
dianlujitao <dianlujitao@lineageos.org>
2019-10-22 21:11:54 +0800

    rosy: Force enable mobile network settings v2
     * AOSP defaults to true but 10 QC telephony extension sets it to false.
     * Restore AOSP behavior by enabling it with a system property which has
       higher priority over global system settings which is used by QCOM w/o
       modifying the jar.
    
    Change-Id: I45854b91bb8cfafabfe811777cda9f1e1840febf

--- prop.mk
+++ prop.mk
@@ -192,6 +192,7 @@ rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
 ro.telephony.call_ring.multiple=false \
 ro.telephony.default_network=22,22 \
 service.qti.ims.enabled=1
+persist.sys.fflag.override.settings_network_and_internet_v2=true
 
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
