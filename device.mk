83501fa2137f7b4b3b94d5c519b8b053de1ca4d4
Akash98Sky <Akash98Sky@users.noreply.github.com>
2019-10-26 10:08:51 +0000

    rosy: power: Build power stats HAL
    fix: Cannot find entry android.hardware.power.stats@1.0::IPowerStats/default in either framework or device manifest.

--- device.mk
+++ device.mk
@@ -296,7 +296,8 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power@1.2-service-qti \
+    android.hardware.power.stats@1.0-service.mock
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
