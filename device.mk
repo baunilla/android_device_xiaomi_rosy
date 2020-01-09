b62fc9b8da9ee1847ec17b86193ffe7dee5cdb44
baunilla <baunilla@tuta.io>
2020-01-09 19:59:08 +0000

    rosy: Uprev Audio HAL to 5.0
    * Add new interface to init.qcom.rc

--- device.mk
+++ device.mk
@@ -77,12 +77,12 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.soundtrigger@2.1-impl \
-    android.hardware.audio@4.0 \
-    android.hardware.audio.common@4.0 \
-    android.hardware.audio.common@4.0-util \
-    android.hardware.audio@4.0-impl \
-    android.hardware.audio.effect@4.0 \
-    android.hardware.audio.effect@4.0-impl \
+    android.hardware.audio@5.0 \
+    android.hardware.audio.common@5.0 \
+    android.hardware.audio.common@5.0-util \
+    android.hardware.audio@5.0-impl \
+    android.hardware.audio.effect@5.0 \
+    android.hardware.audio.effect@5.0-impl \
     audio.a2dp.default \
     audio.primary.msm8953 \
     audio.r_submix.default \
