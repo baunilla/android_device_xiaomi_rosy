330e8b8e0eecf55b9c67ff9b242843ff03095e31
baunilla <baunilla@tuta.io>
2020-01-29 18:58:34 +0000

    rosy: Import custom LiveDisplay 2.0 HAL
    Pulled from LineageOS/android_hardware_lineage_livedisplay/sdm
    
    Cleaned up and adapted for PictureAdjustment (vendor) only:
    
    * Removed AdaptiveBacklight, DisplayModes, Constants, Types, Utils
    * Moved necessary constants to anonymous namespace

--- device.mk
+++ device.mk
@@ -244,7 +244,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
+    vendor.lineage.livedisplay@2.0-service.xiaomi_rosy
 
 # Media
 PRODUCT_COPY_FILES += \
