b229c411f9669914dc5e219944772f5b12539eb8
David Ng <dave@codeaurora.org>
2020-02-11 18:54:03 -0800

    rosy: config.fs: Add sys_boot cap to peripheral manager
    Enable sys_boot capability to allow system reboot
    to peripheral manager.
    
    Change-Id: I04b67543c4d2e541b9699b6faa3337b2638b3257

--- config.fs
+++ config.fs
@@ -47,19 +47,19 @@ caps: BLOCK_SUSPEND NET_ADMIN
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
-caps: NET_BIND_SERVICE
+caps: NET_BIND_SERVICE SYS_BOOT
 
 [system/vendor/bin/pm-service]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
-caps: NET_BIND_SERVICE
+caps: NET_BIND_SERVICE SYS_BOOT
 
 [system/bin/pm-service]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
-caps: NET_BIND_SERVICE
+caps: NET_BIND_SERVICE SYS_BOOT
 
 [vendor/bin/pd-mapper]
 mode: 0755
