ce7cc4c80082300862076be902465b2580ef238a
baunilla <baunilla@tuta.io>
2020-03-01 02:19:05 +0000

    rosy: Fix mobile network settings v2 prop
    Missed new-line escape while cherry-picking 14ccb554a415e7c93327af4bc720506927adf1ce

--- prop.mk
+++ prop.mk
@@ -205,7 +205,7 @@ rild.libargs=-d/dev/smd0 \
 rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
 ro.telephony.call_ring.multiple=false \
 ro.telephony.default_network=22,22 \
-service.qti.ims.enabled=1
+service.qti.ims.enabled=1 \
 persist.sys.fflag.override.settings_network_and_internet_v2=true
 
 # SurfaceFlinger
