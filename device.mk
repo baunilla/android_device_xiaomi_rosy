5931d920a72fc026a8e5a54968d84e4649324563
baunilla <baunilla@tuta.io>
2020-03-11 20:17:04 +0000

    rosy: Build Health HAL 2.0

--- device.mk
+++ device.mk
@@ -207,7 +207,9 @@ PRODUCT_COPY_FILES += \
 
 # Healthd
 PRODUCT_PACKAGES += \
-    chargeonlymode
+    chargeonlymode \
+    android.hardware.health@2.0-impl \
+    android.hardware.health@2.0-service
 
 # HIDL
 PRODUCT_PACKAGES += \
