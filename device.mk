32e6ea2e18199be0fa6d24d3599cb6c32b845f04
PIPIPIG233666 <pig.priv@gmail.com>
2020-03-18 00:18:00 -0400

    rosy: Place a copy of audio platform info intcodec as audio platform info to silence following:     * D platform_info: platform_info_init: Failed to open audio_platform_info.xml, using defaults.
    Change-Id: Icf9bb063d1bef72143c01009da17727f5b6fdefd
    (cherry picked from commit 05d77fa6d6c2874405fadf0abe86556297a856f1)

--- device.mk
+++ device.mk
@@ -111,6 +111,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
+    $(LOCAL_PATH)/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/mixer_paths_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_mtp.xml
 
