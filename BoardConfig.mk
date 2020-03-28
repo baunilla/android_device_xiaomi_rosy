f2b94dc415db7199a3a2a644e6bad7e6308cb227
baunilla <baunilla@tuta.io>
2020-03-28 23:52:19 +0000

    rosy: Import system.prop from LA.UM.8.6.r1-04000-89xx.0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -202,6 +202,9 @@ TARGET_PER_MGR_ENABLED := true
 TARGET_TAP_TO_WAKE_NODE := "/proc/gesture/onoff"
 TARGET_USES_INTERACTION_BOOST := true
 
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+
 # Qualcomm
 BOARD_USES_QCOM_HARDWARE := true
 
