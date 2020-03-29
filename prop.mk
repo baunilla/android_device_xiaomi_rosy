1657136b7831b8ddb4cd50d864eb690d5d53975b
baunilla <baunilla@tuta.io>
2020-03-29 02:55:29 +0100

    rosy: Split vendor and system properties

--- prop.mk
+++ prop.mk
@@ -1,24 +1,13 @@
-#
-# system.prop for rosy
-#
-
 # Apex
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.apex.updatable=false
 
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
-af.fast_track_multiplier=1 \
-audio.deep_buffer.media=true \
 audio.offload.disable=true \
-audio.offload.min.duration.secs=30 \
-audio.offload.video=true \
 persist.vendor.audio.fluence.speaker=false \
 persist.vendor.audio.fluence.voicecall=true \
 persist.vendor.audio.fluence.voicerec=false \
-persist.vendor.btstack.enable.splita2dp=false \
-ro.config.media_vol_steps=25 \
-ro.config.vc_call_vol_steps=15 \
 ro.vendor.audio.sdk.fluencetype=fluence \
 ro.vendor.audio.sdk.ssr=false \
 vendor.audio.flac.sw.decoder.24bit=true \
@@ -43,18 +32,10 @@ vendor.voice.playback.conc.disabled=true \
 vendor.voice.record.conc.disabled=false \
 vendor.voice.voip.conc.disabled=true
 
-# Bluetooth
-PRODUCT_PROPERTY_OVERRIDES += \
-bluetooth.hfp.client=1 \
-qcom.bluetooth.soc=smd \
-ro.bluetooth.hfp.ver=1.7 \
-ro.qualcomm.bt.hci_transport=smd
-
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
 camera.display.lmax=1280x720 \
 camera.display.umax=1920x1080 \
-vendor.camera.hal1.packagelist=com.skype.raider,com.google.android.talk \
 camera.lowpower.record.enable=1 \
 media.camera.ts.monotonic=1 \
 persist.camera.CDS=false \
@@ -65,18 +46,7 @@ persist.camera.isp.clock.optmz=0 \
 persist.camera.stats.test=5 \
 persist.vendor.qti.telephony.vt_cam_interface=1 \
 persist.camera.video.CDS=false \
-vidc.enc.dcvs.extra-buff-count=2 \
-persist.camera.HAL3.enabled=1 \
-vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap
-
-# Cne/Dpm
-PRODUCT_PROPERTY_OVERRIDES += \
-persist.vendor.cne.feature=1 \
-persist.dpm.feature=1
-
-# Coresight
-PRODUCT_PROPERTY_OVERRIDES += \
-persist.debug.coresight.config=stm-events
+persist.camera.HAL3.enabled=1
 
 # Dalvik
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
@@ -89,108 +59,50 @@ dalvik.vm.heapmaxfree=8m
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
-debug.egl.hw=0 \
-debug.enable.sglscale=1 \
-debug.gralloc.enable_fb_ubwc=1 \
-debug.mdpcomp.logs=0 \
-debug.sf.enable_hwc_vds=1 \
-debug.sf.hw=0 \
-debug.sf.latch_unsignaled=1 \
-debug.sf.recomputecrop=0 \
-dev.pm.dyn_samplingrate=1 \
-persist.demo.hdmirotationlock=false \
-persist.hwc.enable_vds=1 \
-persist.hwc.mdpcomp.enable=true \
-ro.opengles.version=196610 \
-ro.qualcomm.cabl=0 \
-ro.vendor.display.cabl=2 \
-sdm.debug.disable_skip_validate=1 \
-vendor.display.disable_skip_validate=1 \
-vendor.display.enable_default_color_mode=1 \
-vendor.gralloc.enable_fb_ubwc=1
+ro.opengles.version=196610
 
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
 drm.service.enabled=true
 
-# Fingerprint
-PRODUCT_PROPERTY_OVERRIDES += \
-persist.qfp=false
-
-# Fm
-PRODUCT_PROPERTY_OVERRIDES += \
-ro.fm.transmitter=false
-
 # Frp
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.frp.pst=/dev/block/bootdevice/by-name/config
 
-# GPS
-PRODUCT_PROPERTY_OVERRIDES += \
-persist.gps.qc_nlp_in_use=1 \
-persist.loc.nlp_name=com.qualcomm.location \
-ro.gps.agps_provider=1
-
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
-av.debug.disable.pers.cache=1 \
-debug.media.codec2=2 \
-media.aac_51_output_enabled=true \
-media.msm8956hw=0 \
 debug.stagefright.omx_default_rank.sw-audio=1 \
 debug.stagefright.omx_default_rank=0 \
-media.stagefright.thumbnail.prefer_hw_codecs=true \
 media.stagefright.audio.sink=280 \
-vendor.mm.enable.qcom_parser=1048575 \
 mm.enable.sec.smoothstreaming=true \
-mmp.enable.3g2=true \
-vendor.audio.hw.aac.encoder=true \
-vendor.vidc.dec.downscalar_height=1088 \
-vendor.vidc.dec.downscalar_width=1920 \
-vendor.vidc.disable.split.mode=1 \
-vendor.vidc.enc.disable.pq=true \
-vendor.vidc.enc.disable_bframes=1 \
-vendor.video.disable.ubwc=1
-
-# Memory optimizations
-PRODUCT_PROPERTY_OVERRIDES += \
-ro.vendor.qti.sys.fw.bservice_enable=true
-
-# Perf
-PRODUCT_PROPERTY_OVERRIDES += \
-ro.vendor.extension_library=libqti-perfd-client.so
-
-# Netmgrd
-PRODUCT_PROPERTY_OVERRIDES += \
-ro.vendor.use_data_netmgrd=true \
-persist.data.netmgrd.qos.enable=true \
-persist.vendor.data.mode=concurrent
+vendor.audio.hw.aac.encoder=true
 
 # Qualcomm
-PRODUCT_PROPERTY_OVERRIDES += \
-ro.vendor.qti.va_aosp.support=1 \
-ro.vendor.qti.va_odm.support=1
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.vendor.qti.va_aosp.support=1
+
+PRODUCT_ODM_PROPERTIES += \
+    ro.vendor.qti.va_odm.support=1
 
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
-DEVICE_PROVISIONED=1 \
 persist.dbg.volte_avail_ovr=1 \
 persist.dbg.vt_avail_ovr=1 \
 persist.dbg.wfc_avail_ovr=1 \
-persist.vendor.radio.apm_sim_not_pwdn=1 \
 persist.radio.multisim.config=dsds \
+persist.vendor.radio.apm_sim_not_pwdn=1 \
+persist.vendor.radio.5g_mode_pref=0 \
 persist.vendor.radio.custom_ecc=1 \
 persist.vendor.radio.procedure_bytes=SKIP \
 persist.vendor.radio.rat_on=combine \
 persist.vendor.radio.sib16_support=1 \
-ril.subscription.types=NV,RUIM \
-rild.libargs=-d/dev/smd0 \
-rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
-ro.telephony.call_ring.multiple=false \
-ro.telephony.default_network=22,22 \
+persist.vendor.ims.disableIMSLogs=1 \
+persist.vendor.ims.disableDebugDataPathLogs=1 \
+persist.vendor.ims.disableDebugLogs=1 \
 service.qti.ims.enabled=1 \
-ro.telephony.iwlan_operation_mode=legacy \
-persist.sys.fflag.override.settings_network_and_internet_v2=true
+persist.sys.fflag.override.settings_network_and_internet_v2=true \
+ro.telephony.call_ring.multiple=false \
+ro.telephony.iwlan_operation_mode=legacy
 
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
@@ -198,15 +110,11 @@ ro.surface_flinger.protected_contents=true
 
 # Time Services
 PRODUCT_PROPERTY_OVERRIDES += \
-persist.timed.enable=true
+persist.vendor.delta_time.enable=true
 
 # Tcp
 PRODUCT_PROPERTY_OVERRIDES += \
 net.tcp.2g_init_rwnd=10
-
-# UI
-PRODUCT_PROPERTY_OVERRIDES += \
-sys.use_fifo_ui=1
 
 # Usb
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -215,8 +123,3 @@ persist.sys.usb.config.extra=none
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
 wifi.interface=wlan0
-
-# WiFi Display
-PRODUCT_PROPERTY_OVERRIDES += \
-persist.debug.wfd.enable=1 \
-persist.sys.wfd.virtual=0
