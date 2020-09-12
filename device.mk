d59ad881357d7830e09e8bab1946ef542d28c083
ShihabZzz <Shihab.riadn@gmail.com>
2020-09-12 21:40:41 +0600

    rosy: Build additional camera packages
    09-12 17:12:40.547  2011  2011 E ims_media_jni: Error loading library lib-imsvt.so: dlopen failed: library "android.hardware.camera.common@1.0.so" not found
    09-12 21:39:08.230  2033  2033 E ims_media_jni: Error loading library lib-imsvt.so: dlopen failed: library "android.hardware.camera.provider@2.4.so" not found
    09-12 22:30:32.164  2005  2005 E ims_media_jni: Error loading library lib-imsvt.so: dlopen failed: library "vendor.qti.hardware.camera.device@1.0.so" not found
    
    Introduced after VNDK current.
    
    Change-Id: Ic5cfb018b1df84a9d8147ec1cbaa1c930f6f1ce4

--- device.mk
+++ device.mk
@@ -118,8 +118,11 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.common@1.0 \
+    vendor.qti.hardware.camera.device@1.0 \
     camera.msm8953 \
     libmm-qcamera \
     Snap \
