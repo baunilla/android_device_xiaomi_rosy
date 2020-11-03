e4750936139277c18ad2193a8e76aa7fbcd477aa
Arvind Kumar <karvin@codeaurora.org>
2020-11-03 10:10:23 +0530

    rosy: Add vndservicemanager explicitly
    This is now added to product_packages based on shipping_api which is
    not yet supported on few targets. Hence adding it explicitly
    
    Change-Id: I4bc6d43627e92e1d7a7d3fe82ec6e58fc5fdaca7

--- device.mk
+++ device.mk
@@ -437,6 +437,10 @@ PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite.so \
 
+# vndservicemanager
+PRODUCT_PACKAGES += \
+    vndservicemanager
+
 # Whitelisted app
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
