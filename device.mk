d1ecb3285516192640274da3edb4370dfdf2053c
baunilla <baunilla@tuta.io>
2020-12-19 16:35:48 +0000

    rosy: doze: Drop custom Doze package
    * Inconsistent w/ default AOD settings

--- device.mk
+++ device.mk
@@ -156,10 +156,6 @@ PRODUCT_PACKAGES += \
     libgenlock \
     libtinyxml
 
-# Doze mode
-PRODUCT_PACKAGES += \
-    XiaomiDoze
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl:64 \
