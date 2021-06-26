08d5425711046f1efcbf8b0e4679913cb5bb49b3
Albert I <kras@raphielgang.org>
2021-06-26 15:30:31 +0800

    rosy: Inherit several Android Go configurations
    Beneficial for everyone, even on non-Go devices.
    
    Change-Id: I088fd00500ce10df5f413cb762aaed2ec00f5dfc

--- device.mk
+++ device.mk
@@ -20,6 +20,11 @@ $(call inherit-product, vendor/xiaomi/rosy/rosy-vendor.mk)
 TARGET_BOARD_PLATFORM := msm8953
 TARGET_BOARD_SUFFIX := _64
 
+# Use a profile based boot image for this device. Note that this is currently a
+# generic profile and not Android Go optimized.
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 # APEX
 PRODUCT_COMPRESSED_APEX := false
 
