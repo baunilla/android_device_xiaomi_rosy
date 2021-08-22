e69c1e7e87e04f9d1a2487829abb4cb713a568b5
Mathieu Chartier <mathieuc@google.com>
2021-08-22 16:31:23 +0800

    common: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3

--- device.mk
+++ device.mk
@@ -25,6 +25,9 @@ TARGET_BOARD_SUFFIX := _64
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+ 
 # APEX
 PRODUCT_COMPRESSED_APEX := false
 
