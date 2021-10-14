8838a59cc1df8aaf4ec1290ace2bdbb1480d6ed7
baunilla <baunilla@tuta.io>
2021-10-14 22:20:11 +0100

    rosy: lineagehw: Drop custom livedisplay
    * Also sepolicy rules

--- device.mk
+++ device.mk
@@ -259,10 +259,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.xiaomi
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service.xiaomi_rosy
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml::$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
