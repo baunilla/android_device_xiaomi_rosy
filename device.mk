a0b9b027edcf51f849a4aa080cd1d0c0511dd4de
baunilla <baunilla@tuta.io>
2021-11-09 22:03:26 +0000

    rosy: Update legacy blobs from LA.UM.8.6.2.r1-08600-89xx.0
    * Pin Gatekeeper, Graphics, Keystore, Perf and Soundtrigger blobs.
    * Pin libsdm-color.so from LA.UM.8.6.r1-03400-89xx.0 (Nightlight fix)
    * Uprev Perf to v2.1 - Perf configs from LA.UM.8.6.2.r1-08600-89xx.0

--- device.mk
+++ device.mk
@@ -303,7 +303,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
     android.hardware.power.stats@1.0-service.mock \
-    vendor.qti.hardware.perf@2.0.vendor
+    vendor.qti.hardware.perf@2.1.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
