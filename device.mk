ebf4c57e7f3f7fe4eb1f4fd8c4ca6d83f468e62c
baunilla <49725678+baunilla@users.noreply.github.com>
2021-11-14 22:49:07 +0000

    rosy: Move vendor properties to makefile
    * Move some props to PRODUCT_VENDOR_PROPERTIES.

--- device.mk
+++ device.mk
@@ -318,6 +318,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+# Properties
+-include device/xiaomi/rosy/prop.mk
+
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
---
+++ prop.mk
@@ -0,0 +1,154 @@
+
+# Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+audio.offload.disable=true \
+persist.vendor.audio.fluence.speaker=false \
+persist.vendor.audio.fluence.voicecall=true \
+persist.vendor.audio.fluence.voicerec=false \
+ro.vendor.audio.sdk.fluencetype=fluence \
+ro.vendor.audio.sdk.ssr=false \
+vendor.audio.feature.compr_voip.enable=true \
+vendor.audio.feature.compress_meta_data.enable=true \
+vendor.audio.flac.sw.decoder.24bit=true \
+vendor.audio.offload.buffer.size.kb=64 \
+vendor.audio.offload.gapless.enabled=true \
+vendor.audio.offload.multiaac.enable=true \
+vendor.audio.offload.multiple.enabled=false \
+vendor.audio.offload.passthrough=false \
+vendor.audio.offload.track.enable=true \
+vendor.audio.parser.ip.buffer.size=262144 \
+vendor.audio.playback.mch.downsample=true \
+vendor.audio.pp.asphere.enabled=false \
+vendor.audio.read.wsatz.type=true \
+vendor.audio.safx.pbe.enabled=true \
+vendor.audio.tunnel.encode=false \
+vendor.audio.use.sw.alac.decoder=true \
+vendor.audio.use.sw.ape.decoder=true \
+vendor.audio_hal.period_size=192 \
+vendor.fastrpc.disable.adsprpcd_sensorspd.daemon=1 \
+vendor.voice.conc.fallbackpath=deep-buffer \
+vendor.voice.path.for.pcm.voip=true \
+vendor.voice.playback.conc.disabled=true \
+vendor.voice.record.conc.disabled=false \
+vendor.voice.voip.conc.disabled=true
+
+# Bluetooth
+PRODUCT_PROPERTY_OVERRIDES += \
+vendor.qcom.bluetooth.soc=pronto \
+persist.vendor.qcom.bluetooth.enable.splita2dp=false \
+ro.vendor.bluetooth.wipower=false
+
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+camera.display.lmax=1280x720 \
+camera.display.umax=1920x1080 \
+camera.lowpower.record.enable=1 \
+media.camera.ts.monotonic=1 \
+persist.camera.CDS=false \
+persist.camera.HAL3.enabled=1 \
+persist.camera.dual.camera=0 \
+persist.camera.eis.enable=1 \
+persist.camera.gyro.disable=0 \
+persist.camera.isp.clock.optmz=0 \
+persist.camera.stats.test=5 \
+persist.camera.video.CDS=false \
+persist.vendor.qti.telephony.vt_cam_interface=1
+
+# Charger
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.charger.disable_init_blank=true \
+ro.charger.enable_suspend=true
+
+# Data
+PRODUCT_VENDOR_PROPERTIES += \
+ro.com.android.mobiledata=false
+
+# Dalvik
+PRODUCT_VENDOR_PROPERTIES += \
+dalvik.vm.heapstartsize=16m \
+dalvik.vm.heapgrowthlimit=192m \
+dalvik.vm.heapsize=512m \
+dalvik.vm.heaptargetutilization=0.75 \
+dalvik.vm.heapminfree=2m \
+dalvik.vm.heapmaxfree=8m
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.opengles.version=196610 \
+ro.hardware.vulkan=adreno \
+ro.hardware.egl=adreno
+
+# DPM
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.vendor.dpmhalservice.enable=1
+
+# DRM
+PRODUCT_PROPERTY_OVERRIDES += \
+drm.service.enabled=true
+
+# FRP
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.frp.pst=/dev/block/bootdevice/by-name/config
+
+# Media
+PRODUCT_PROPERTY_OVERRIDES += \
+debug.stagefright.omx_default_rank.sw-audio=1 \
+debug.stagefright.omx_default_rank=0 \
+media.stagefright.audio.sink=280 \
+mm.enable.sec.smoothstreaming=true \
+vendor.audio.hw.aac.encoder=true
+
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.dbg.volte_avail_ovr=1 \
+persist.dbg.vt_avail_ovr=1 \
+persist.dbg.wfc_avail_ovr=1 \
+persist.radio.multisim.config=dsds \
+persist.vendor.data.iwlan.enable=true \
+persist.vendor.ims.disableDebugDataPathLogs=1 \
+persist.vendor.ims.disableDebugLogs=1 \
+persist.vendor.ims.disableIMSLogs=1 \
+persist.vendor.radio.add_power_save=1 \
+persist.vendor.radio.apm_sim_not_pwdn=1 \
+persist.vendor.radio.5g_mode_pref=0 \
+persist.vendor.radio.custom_ecc=1 \
+persist.vendor.radio.enableadvancedscan=false \
+persist.vendor.radio.procedure_bytes=SKIP \
+persist.vendor.radio.rat_on=combine \
+persist.vendor.radio.sib16_support=1 \
+ro.telephony.call_ring.multiple=false \
+ro.telephony.iwlan_operation_mode=legacy \
+service.qti.ims.enabled=1
+
+# Recovery
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.vendor.recovery_update=false
+
+# SoC
+PRODUCT_VENDOR_PROPERTIES += \
+ro.soc.manufacturer=Qualcomm \
+ro.soc.model=MSM8953
+
+# SurfaceFlinger
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+ro.surface_flinger.max_virtual_display_dimension=4096 \
+ro.surface_flinger.protected_contents=true
+
+# Time Services
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.vendor.delta_time.enable=true
+
+# TCP
+PRODUCT_PROPERTY_OVERRIDES += \
+net.tcp.2g_init_rwnd=10
+
+# USB
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.vendor.usb.config.extra=none
+
+# WiFi
+PRODUCT_PROPERTY_OVERRIDES += \
+wifi.interface=wlan0
+
