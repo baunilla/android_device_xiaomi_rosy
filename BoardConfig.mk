dcbf40a62b37cd1be5fdbe66e8013794ae6522c1
baunilla <baunilla@tuta.io>
2021-11-16 17:03:42 +0000

    rosy: Switch to SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS
    * BOARD_PLAT_PRIVATE_SEPOLICY_DIR has been deprecated.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -199,7 +199,7 @@ VENDOR_SECURITY_PATCH := 2021-02-01
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
