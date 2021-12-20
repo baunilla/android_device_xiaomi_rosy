31b127c951f3487fd2c3b6b64b2b66811c3359aa
Wang Han <416810799@qq.com>
2021-12-20 16:45:33 +0800

    rosy: configs: Import gpfspath_oem_config.xml
    * Opened by libdrmfs.so. Add it to be safe better than sorry.
    
    Change-Id: I46f33fba4591750a3c932e15b2b80a3c071ee4dd

--- device.mk
+++ device.mk
@@ -186,6 +186,9 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey \
     android.hardware.drm@1.4.vendor
 
+PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/configs/gpfspath_oem_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/gpfspath_oem_config.xml
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     gdxbacktrace \
