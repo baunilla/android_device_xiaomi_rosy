7ecaf01fe881d175f35e72df41da8352c973a737
kubersharma001 <kubersharma001@gmail.com>
2022-01-04 20:40:59 +0000

    rosy: UpRev Radio to v1.5
    * enables additional features over 1.4 like AOSP sim toggle
    
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- device.mk
+++ device.mk
@@ -382,12 +382,9 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.4 \
     android.hardware.radio@1.5.vendor \
-    android.hardware.radio.config@1.2 \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
-    android.hardware.secure_element@1.0 \
     android.hardware.secure_element@1.2.vendor \
     librmnetctl \
     librmnetctl.vendor \
