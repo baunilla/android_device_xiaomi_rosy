b501568e7774e08f3e7624127ae52377f2bbd0a0
Robert Shih <robertshih@google.com>
2022-02-14 16:02:55 -0800

    rosy: enable AIDL DRM HALs
    Bug: 219538389
    Test: atest GtsMediaTestCases
    Change-Id: Iaf321aabf7f0ef3d86c518982781d95c4a648506

--- device.mk
+++ device.mk
@@ -183,7 +183,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm-service.clearkey \
     android.hardware.drm@1.4.vendor
 
 PRODUCT_COPY_FILES += \
