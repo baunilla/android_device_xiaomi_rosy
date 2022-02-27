0b580349cad3ff45f1e3d99a3a2135b9249e6e48
baunilla <baunilla@tuta.io>
2022-02-27 20:25:49 +0000

    rosy: Update permissions from LA.UM.10.6.2.r1-01100-89xx.0
    * Move qti_whitelist.xml to permissions folder.

--- device.mk
+++ device.mk
@@ -444,7 +444,7 @@ PRODUCT_PACKAGES += \
 
 # Whitelisted app
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
+    $(LOCAL_PATH)/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
     $(LOCAL_PATH)/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml
 
 # Wifi
