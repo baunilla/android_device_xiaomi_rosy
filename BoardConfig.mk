8b9817b2f0be2f6d04f5aa72988775faafd07c88
baunilla <baunilla@tuta.io>
2022-07-31 19:45:18 +0100

    Revert "[dnm]: selinux permissive"
    This reverts commit 63402638ae3ef105da90a43917e951b83f4e0ff4.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,7 +41,6 @@ BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.usbconfigfs=true androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000 loop.max_part=7
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
