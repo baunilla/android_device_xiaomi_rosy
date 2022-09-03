e6bb553d4983e81bd0ac94c3f05f08e901af7948
baunilla <baunilla@tuta.io>
2022-09-03 20:09:01 +0100

    rosy: Fix newline escape in device.mk
    * In libprotobuf copy from vndk

--- device.mk
+++ device.mk
@@ -431,7 +431,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so \
-    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite.so
 
 # vndservicemanager
 PRODUCT_PACKAGES += \
