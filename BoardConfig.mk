acbe535c5b2d044133724a9c38c9f67902d14b9c
baunilla <baunilla@tuta.io>
2023-01-09 19:15:58 +0000

    rosy: Update blobs from FP3
    * From FP3:11/8901.4.A.0019.2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -180,7 +180,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
 # Security Patch Level
-VENDOR_SECURITY_PATCH := 2022-10-05
+VENDOR_SECURITY_PATCH := 2022-12-05
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
