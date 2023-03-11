ec1f0c1229ef273eed4dd624064d618d83efdce2
me-cafebabe <me.cafebabe@gmail.com>
2023-03-12 05:33:34 +0800

    rosy: Build Camera2 app explicitly
    Change-Id: I75c285ae7807659e20cd0f2757da0e4f21ad80b9

--- device.mk
+++ device.mk
@@ -151,7 +151,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor \
     camera.msm8953 \
     libshimcam \
-    libgui_vendor:32
+    libgui_vendor:32 \
+    Camera2
 
 # Cgroup and task_profiles
 PRODUCT_COPY_FILES += \
