300fc47cc2837efa188cf61f4520ca08982ed264
baunilla <baunilla@tuta.io>
2023-06-09 18:53:14 +0100

    rosy: Drop Health HAL recovery implementation
     * currently not shipping recovery

--- device.mk
+++ device.mk
@@ -224,10 +224,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
     $(LOCAL_PATH)/configs/gps/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
 
-# Healthd
+# Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl:64 \
-    android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
 
 # HIDL
