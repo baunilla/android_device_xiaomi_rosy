c66702f05dc70337c911fb3e1cf89957341bd05c
basamaryan <basam.aryan@gmail.com>
2023-10-07 15:17:04 -0400

    rosy: Build libprotobuf-cpp-full-3.9.1-vendorcompat
    For now this is implicitly built by lineage specific
    libprotobuf-cpp-lite-3.9.1-vendorcompat lib, but in the future this
    might not be the case.
    
    Change-Id: I724e933e046cbf631c078321d55550b96ca998da

--- device.mk
+++ device.mk
@@ -330,6 +330,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-vendorcompat:64 \
     libprotobuf-cpp-lite-vendorcompat:64 \
+    libprotobuf-cpp-full-3.9.1-vendorcompat \
     libprotobuf-cpp-lite-3.9.1-vendorcompat
     
 # Public Libraries
