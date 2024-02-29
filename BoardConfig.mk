b8d798c6d85fec55053fbe00dc9305b7387b269b
Yumi Yukimura <me.cafebabe@gmail.com>
2024-02-29 21:29:05 +0800

    rosy: Drop custom ChargingControl path
    * The path is added to default paths after I7aecc713028293253d3efaa8e5b7bf8a665bc5af
    
    Change-Id: Iaba9dd1c7d672030af7c38eac7c25a8e8b4ef0e7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -137,9 +137,6 @@ DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
 # Keymaster
 TARGET_PROVIDES_KEYMASTER := true
 
-# Lineage Health
-TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH := /sys/class/power_supply/battery/battery_charging_enabled
-
 # Media
 TARGET_USES_MEDIA_EXTENSIONS := true
 
