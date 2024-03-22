f0ca1c3b6b164bd3668f94878cc9f2d3b13f65ff
Aaron Kling <webgeek1234@gmail.com>
2024-03-22 08:05:53 -0500

    rosy: Uprev usb hal to 1.3
    Change-Id: I5d34c71663b31fe03b0d28f8c76e5a4f8cd2385c

--- device.mk
+++ device.mk
@@ -416,7 +416,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.3-service.basic
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
